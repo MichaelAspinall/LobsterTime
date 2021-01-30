642114304c14249b6829f3cb58d50c44d9e92e5a
Oneknown <theoneknownsoldier@gmail.com>
2021-01-30 15:05:55 -0500

    Camera now follows player

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public Transform PlayerTransform;
+    private Vector2 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = new Vector2(2.0f, 2.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         //TODO
-        //transform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y);
+        transform.position = new Vector3(PlayerTransform.position.x + offset.x, PlayerTransform.position.y + offset.y, -10.0f);
     }
 }
